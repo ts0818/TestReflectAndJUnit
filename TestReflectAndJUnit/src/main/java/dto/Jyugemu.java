defdbf0a67206a23b5f15d23f994a4a18b7b6233
ts0818 <toshi888.8.888@gmail.com>
2019-06-15 22:26:40 +0900

    JavaのリフレクションのJunitでのテスト

---
+++ TestReflectAndJUnit/src/main/java/dto/Jyugemu.java
@@ -0,0 +1,105 @@
+package dto;
+
+public class Jyugemu {
+
+  private String name01;
+  private String name02;
+  private String name03;
+  private String name04;
+  private String name05;
+  private String name06;
+  private String name07;
+  private String name08;
+  private String name09;
+  private String name10;
+  private String name11;
+
+  public String getName01() {
+    return name01;
+  }
+
+  public void setName01(String name01) {
+    this.name01 = name01;
+  }
+
+  public String getName02() {
+    return name02;
+  }
+
+  public void setName02(String name02) {
+    this.name02 = name02;
+  }
+
+  public String getName03() {
+    return name03;
+  }
+
+  public void setName03(String name03) {
+    this.name03 = name03;
+  }
+
+  public String getName04() {
+    return name04;
+  }
+
+  public void setName04(String name04) {
+    this.name04 = name04;
+  }
+
+  public String getName05() {
+    return name05;
+  }
+
+  public void setName05(String name05) {
+    this.name05 = name05;
+  }
+
+  public String getName06() {
+    return name06;
+  }
+
+  public void setName06(String name06) {
+    this.name06 = name06;
+  }
+
+  public String getName07() {
+    return name07;
+  }
+
+  public void setName07(String name07) {
+    this.name07 = name07;
+  }
+
+  public String getName08() {
+    return name08;
+  }
+
+  public void setName08(String name08) {
+    this.name08 = name08;
+  }
+
+  public String getName09() {
+    return name09;
+  }
+
+  public void setName09(String name09) {
+    this.name09 = name09;
+  }
+
+  public String getName10() {
+    return name10;
+  }
+
+  public void setName10(String name10) {
+    this.name10 = name10;
+  }
+
+  public String getName11() {
+    return name11;
+  }
+
+  public void setName11(String name11) {
+    this.name11 = name11;
+  }
+
+}
